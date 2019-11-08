6cbd09291c94f342d76b39cc4fe0f78d7f1c2657
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-11-08 20:56:31 +0100

    Buttons component

---
+++ src/Buttons.svelte
@@ -0,0 +1,9 @@
+<style>
+
+</style>
+
+<div class="row">
+  <button class="btn-danger btn-lg">New Game</button>
+  <button class="btn-primary btn-sm">Undo</button>
+  <button class="btn-success btn-sm">Replay</button>
+</div>
