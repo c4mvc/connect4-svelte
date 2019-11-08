4c1b429453485459b1c4699bd30d3df639b37927
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-11-08 20:58:35 +0100

    GameArea Component

---
+++ src/GameArea.svelte
@@ -0,0 +1,47 @@
+<script>
+  
+</script>
+
+<style>
+  .CircleBase {
+    border-radius: 50%;
+  }
+
+  .CircleRed {
+    width: 50px;
+    height: 50px;
+    background: red;
+    border: 3px solid red;
+    margin: auto;
+  }
+
+  .CircleYellow {
+    width: 50px;
+    height: 50px;
+    background: yellow;
+    border: 3px solid yellow;
+    margin: auto;
+  }
+
+  .CursorArea {
+    width: 70px;
+    height: 70px;
+    padding-top: 45px;
+    border: none;
+    float: left;
+  }
+</style>
+
+<div class="row">
+  <div class="row">
+    <div class="col-xs-9">
+
+      <div class="bottom-buffer">
+        <div class="clearfix area-width">
+
+      
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
