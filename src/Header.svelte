bb4d54ce28bac9f433b7e98024acc456f326b21b
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-11-08 20:56:10 +0100

    Header component

---
+++ src/Header.svelte
@@ -0,0 +1,14 @@
+<style>
+  h1 {
+    color: red;
+    font-family: Verdana;
+    font-size: 3em;
+    background-color: lavender;
+    box-shadow: 5px 5px 5px blueviolet;
+    display: inline-block;
+  }
+</style>
+
+<div className="row">
+  <h1>Connect4 Game</h1>
+</div>
