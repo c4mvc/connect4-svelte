6e9da6d2926f1add7fb36a0624b349fa5d2be9d8
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-11-08 20:57:29 +0100

    PlayerLabel component

---
+++ src/PlayerLabel.svelte
@@ -0,0 +1,32 @@
+<style>
+  .CircleBase {
+    border-radius: 50%;
+  }
+
+  .CircleRed {
+    width: 50px;
+    height: 50px;
+    background: red;
+    border: 3px solid red;
+    margin: auto;
+  }
+
+  .CircleYellow {
+    width: 50px;
+    height: 50px;
+    background: yellow;
+    border: 3px solid yellow;
+    margin: auto;
+  }
+</style>
+
+<div class="row">
+  <div class="col-md-3">
+    <h3>Player1</h3>
+    <div class="CircleBase CircleRed" />
+  </div>
+  <div class="col-md-3">
+    <h3>Player2</h3>
+    <div class="CircleBase CircleYellow" />
+  </div>
+</div>
