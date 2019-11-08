185cfdf69df305626ca2ea38750c37a03d2b94e0
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-11-08 21:01:13 +0100

    Main app file

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Header from "./Header.svelte";
+  import Buttons from "./Buttons.svelte";
+  import PlayerLabel from "./PlayerLabel.svelte";
+  import GameArea from "./GameArea.svelte";
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<div class="container">
+  <Header />
+
+  <Buttons />
+
+  <PlayerLabel />
+
+  <GameArea />
+</div>
+
+<!-- <h1>Hello {name}!</h1> -->
